8dc38919138b67228373925e91ade7533e8f600c
rahul <113882096+wraith2009@users.noreply.github.com>
2023-04-13 15:38:44 +0530

    Create EqualityAndRelational.kt

---
+++ EqualityAndRelational.kt
@@ -0,0 +1,26 @@
+package com.example.secoundproject
+
+fun main()
+{
+    /*
+    1. ==
+    2. !=
+    3. >
+    4. <
+    5. >=
+    6 <=
+    */
+
+    var a:Int=2
+    var b:Int=5
+
+    println("a==b : "  +(a == b))
+    println("a!=b : "  +(a != b))
+    println("a>b : "  +(a > b))
+    println("a>=b : "  +(a >= b))
+    println("a<b : "  +(a < b))
+    println("a<=b : "  +(a <= b))
+
+
+
+}
